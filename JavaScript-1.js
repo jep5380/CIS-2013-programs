e8f95cc63a451f2e3eab7976721f6f3193d45854
jep5380 <54776445+jep5380@users.noreply.github.com>
2019-09-20 18:55:10 +0000

    Delete JavaScript-1.js

--- JavaScript-1.js
+++
@@ -1,7 +0,0 @@
-     var floatMiles;
-     var floatGallons;
-     var floatMpg;
-     floatMiles = parseFloat(prompt("Enter miles driven"));
-     floatGallons = parseFloat(prompt("Enter gallons of gas used"));
-     floatMpg = miles/gallons;
-     alert("Miles per gallon = " + varMpg);
