2402ca2182e12c411533602db26fe72d12240601
jep5380 <54776445+jep5380@users.noreply.github.com>
2019-10-04 13:29:05 -0500

    Add files via upload

---
+++ 2013grade.js
@@ -0,0 +1,56 @@
+//Declare variables intGradeOption, floatTotalPts, floatHwPts, floatMidPts, floatFinPts, stringFinalGrade
+//Get user input to determine grade
+//Calculate floatTotalPts (sum of HW and exam pts)
+//Get user input on whether the course is audit pass/fail (1) or letter grade (2)
+//If the grade option is audit, evaluate total score based upon 80% cutoff scorefor pass/fail
+//Else determine letter grade based upon the typical A = 90%, B - 80%, etc.
+//Output stringFinalGrade
+
+var intGradeOption, floatTotalPts, floatHwPts, floatMidPts, floatFinPts, stringFinalGrade;
+
+floatHwPts = parseFloat(prompt("Please enter final HW pts (0-30):"));
+
+floatMidPts = parseFloat(prompt("Please enter final MidTerm points: "));
+
+floatFinPts = parseFloat(prompt("Please enter final points: " ));
+
+floatTotalPts = parseFloat(??);
+
+intGradeOption = parseFloat(prompt("Enter 1 for pass/fail grading option, enter 2 for regular grading option: "));
+
+if (intGradeOption===1)
+{
+    if(floatTotalPts >= 80)
+    {
+        return "pass";
+    }
+    else{
+        return "fail";
+    }
+}
+else if (intGradeOption===2)
+{
+    switch(floatTotalPts)
+    {
+        case floatTotalPts>=90:
+            return "A";
+        case floatTotalPts>=80&&floatTotalPts<90:
+            return "B";
+        case floatTotalPts>=70&&floatTotalPts<80:
+            return "C";
+        case floatTotalPts>=60&&floatTotalPts<70:
+            return "D";
+        case floatTotalPts<60:
+            return "F";
+
+    }
+    
+}
+
+
+
+
+
+
+
+alert("Your final grade is: " +stringFinalGrade);
