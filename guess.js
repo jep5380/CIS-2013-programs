0e9934586e735cb5f341a4f8f4a0a023f696c410
jep5380 <54776445+jep5380@users.noreply.github.com>
2019-11-04 20:16:53 -0600

    Update guess.js

--- guess.js
+++ guess.js
@@ -15,62 +15,16 @@ while(intMin < 0 || isNaN(intMin)
 {
     intMin = parseInt(prompt("Choose the lowest number (but no lower than 0) in your guessing range"));
 }
+/* the following section prompts the user to enter the high number of their guessing range
+ * and then validates that the user entered an actual number and make sure that the
+ * number is at least 2 more than the minimum (so that there is some guessing involved).
+*/
 intMax = parseInt(prompt("Choose the highest number in your guessing range(must be at least 2 more " +intMin + "):"));
 
 while(isNaN(intMax) || intMax < intMin +2)
 {
     intMax = parseInt(prompt("Choose the highest number in your guessing range(must be at least 2 more than your lowest number):"));
 }
-intGuess = parseInt(prompt("what is your guess on the random number between " + intMin + " and " + intMax));
-while(intGuess < intMin || intGuess > intMax)
-{
-    intGuess = parseInt(prompt("Your guess is not between " +intMin + " and " + intMax));   
-}
-intRandom = parseInt(Math.floor(Math.random()*(intMax-intMin+1))+intMin);
-intCount = 1;
-while(intGuess != intRandom)
-{
-    if( intGuess < intRandom)
-    {
-        alert("guess is too low");
-        intGuess = parseInt(prompt("what is your guess on the random number between " + intMin + " and " + intMax));
-        while(intGuess < intMin || intGuess > intMax)
-        {
-            intGuess = parseInt(prompt("Your guess is not between " +intMin + " and " + intMax));   
-        }
-    }
-    else
-    {
-        alert("guess is too high");
-        intGuess = parseInt(prompt("what is your guess on the random number between " + intMin + " and " + intMax));
-        while(intGuess < intMin || intGuess > intMax)
-        {
-            intGuess = parseInt(prompt("Your guess is not between " + intMin + " and " + intMax));   
-        }
-    }
-    intCount++;
-}
-alert("Congratulations!! you guessed the correct number (" + intRandom + ") and it only took you " + intCount + "attempts!");
-
-
-
-
-
-
-
-/* the following section prompts the user to enter the high number of their guessing range
- * and then validates that the user entered an actual number and make sure that the
- * number is at least 2 more than the minimum (so that there is some guessing involved).
-*/
-
-
-
-
-
-
-
-
-
 /*The following line of code generates the random number to be used in the guessing game.
  * Math.floor rounds the random number down to the nearest integer
  * Math.random() generates a random number between 0 and 1
@@ -78,37 +32,43 @@ alert("Congratulations!! you guessed the correct number (" + intRandom + ") and 
  * the addition of + intMin provides the floor for the random number 
  */
 intRandom = parseInt (Math.floor(Math.random()*(intMax-intMin+1))+intMin);
-
-
 // set the loop counter
-
-
-
+intCount = 1;
 /* the following section prompts the user to enter their guess
  * and then validates that the user entered an actual number and makes sure that the
  * number is between the allowed max and min number choices.
 */
-
-
-
-
-
-
+intGuess = parseInt(prompt("what is your guess on the random number between " + intMin + " and " + intMax));
+while(intGuess < intMin || intGuess > intMax)
+{
+    intGuess = parseInt(prompt("Your guess is not between " +intMin + " and " + intMax));   
+}
 /* The following section provides the main loop and logic for the program.
  * The user's guess is compared to the randomly generated number and feedback
  * is given based upon whether the guess is higher or lower. The loop exits when
  * the user chooses the correct number. Each time through the loop updates the loop counter.
  */
 
- 
- 
- 
- 
- 
- 
- 
- 
-
+while(intGuess != intRandom)
+{
+    if( intGuess < intRandom)
+    {
+        intGuess = parseInt(prompt("Guess is too low. What is your guess on the random number between " + intMin + " and " + intMax));
+        while(intGuess < intMin || intGuess > intMax)
+        {
+            intGuess = parseInt(prompt("Your guess is not between " +intMin + " and " + intMax));   
+        }
+    }
+    else
+    {
+        intGuess = parseInt(prompt("Guess is too high. What is your guess on the random number between " + intMin + " and " + intMax));
+        while(intGuess < intMin || intGuess > intMax)
+        {
+            intGuess = parseInt(prompt("Your guess is not between " + intMin + " and " + intMax));   
+        }
+    }
+    intCount++;
+}
 // provides final output upon successful guessing
 alert("Congratulations!!! You guessed the correct number (" + intRandom +")\n" +
 		" and it only took you " + intCount + " attempts!");
